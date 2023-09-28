d6686071b876253f1c040eb43735db07c74c8d40
CaioSilva26 <caiosilvadeazevedo@gmail.com>
2023-09-28 08:52:12 -0300

    lmaooo

--- app/src/main/java/com/example/android2/MainActivity.kt
+++ app/src/main/java/com/example/android2/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Android2Theme {
-        Greeting("Android")
+        Greeting("droid")
     }
 }
